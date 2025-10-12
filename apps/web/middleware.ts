30cbc19c977e064e1b38d3b33b6d8b04842ac9c1
Itay Sagui <itsagui@microsoft.com>
2025-10-12 22:38:25 +0300

    Fixed robots.txt

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -8,7 +8,7 @@ import {
 export const config = {
   // matcher tells Next.js which routes to run the middleware on. This runs the
   // middleware on all routes except for static assets and Posthog ingest
-  matcher: ['/((?!_next/static|_next/image|ingest|favicon.ico).*)'],
+  matcher: ['/((?!_next/static|_next/image|ingest|favicon.ico|robots.txt|sitemap.xml).*)'],
 };
 
 const middleware = (request: NextRequest) => {
