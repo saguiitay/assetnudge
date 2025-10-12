ad3e3b69e59a0e6d27f289d3661aae24ed95e1e4
Itay Sagui <itsagui@microsoft.com>
2025-10-12 22:51:14 +0300

    Improve sitemap

--- apps/web/app/robots.ts
+++ apps/web/app/robots.ts
@@ -1,7 +1,9 @@
 import type { MetadataRoute } from 'next'
  
 export default function robots(): MetadataRoute.Robots {
-  const siteUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL || 'http://localhost:3000'
+  const siteUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : 'http://localhost:3000'
 
   return {
     rules: {
