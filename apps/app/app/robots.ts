e8cbcce8ddece364305eb65430adc48e2caa4255
Itay Sagui <itsagui@microsoft.com>
2025-10-14 15:09:10 +0300

    Fixed

--- apps/app/app/robots.ts
+++ apps/app/app/robots.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next'
 export default function robots(): MetadataRoute.Robots {
   const siteUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
     ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-    : 'http://localhost:3000'
+    : 'http://localhost:3001'
 
   return {
     rules: {
